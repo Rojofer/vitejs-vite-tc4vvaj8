cbe9ca9ee7340de446439434e05207aa9e9ba047
Rojofer <110467691+Rojofer@users.noreply.github.com>
2026-05-30 00:00:03 -0300

    Update VistaAuditoria.jsx

--- src/vistas/VistaAuditoria.jsx
+++ src/vistas/VistaAuditoria.jsx
@@ -102,7 +102,7 @@ const VistaAuditoria = ({ insumos, reclamos, currentUser, formatearFecha, obtene
         // ==============================================================================
         // ⚠️ IMPORTANTE FERNANDO: Pegá acá la URL de tu Web App de Google Apps Script ⚠️
         // ==============================================================================
-        const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlhtUtS1goq_-QYW9-i-nqiwcUuseP9EnTuUOoV97Lt2F8beqdUJCzeY5VhL3baaA-/exec"; 
+        const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzcljy-3vbfEzsgy6la05gQ3TOiKhlFweIpuno15Rx1LqgcSgWXlSS63KuWISerrOXC/exec"; 
         
         await fetch(SCRIPT_URL, {
           method: 'POST',
